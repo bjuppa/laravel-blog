d56d21bb646ff12efcebec6110566513d28c36df
Björn Nilsved <nilsved@gmail.com>
2017-09-28 15:01:07 +0200

    Provide a base for a service provider

---
+++ src/BlogServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bjuppa\LaravelBlog;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlogServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
