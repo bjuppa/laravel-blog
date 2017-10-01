3777b85df1b888574e873715b83d3e7bfa829fce
Björn Nilsved <nilsved@gmail.com>
2017-10-01 20:01:52 +0200

    Create migration for blog entries table

---
+++ database/migrations/bjuppa_laravel_blog_001_create_blog_entries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_entries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_entries');
+    }
+}
