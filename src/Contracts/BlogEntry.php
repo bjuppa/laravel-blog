5495b9147673af1123f286e5e2616175c419119e
Björn Nilsved <nilsved@gmail.com>
2017-10-02 10:22:41 +0200

    Add todo

--- src/Contracts/BlogEntry.php
+++ src/Contracts/BlogEntry.php
@@ -2,6 +2,7 @@
 
 namespace Bjuppa\LaravelBlog\Contracts;
 
+// TODO: Methods could have the blog identifier (or perhaps a Blog object built in the service provider) as input parameter and use it to return different values if the model is used for several blogs.
 
 interface BlogEntry
 {
