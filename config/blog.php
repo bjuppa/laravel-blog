934dad88656772e91e35b5b8c8b6697daf6697c5
Björn Nilsved <nilsved@gmail.com>
2017-10-03 16:48:51 +0200

    Improve docs

--- config/blog.php
+++ config/blog.php
@@ -16,7 +16,7 @@ return [
     | Blog entries are stored in a table called `blog_entries` by default. If
     | you want another name for the table you can set it here. This table
     | name will be used by the registered migrations and the BlogEntry
-    | model (and its descendants should you extend it).
+    | model (as well as its descendants, should you extend it).
     |
     */
 
