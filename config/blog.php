7ee1e9ca77f7ce63155bdafbb8608cf6bfb188bf
Björn Nilsved <nilsved@gmail.com>
2017-10-03 10:30:27 +0200

    Set basic config values

--- config/blog.php
+++ config/blog.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-
+    'blogs' => [
+        [
+            'id' => 'default',
+            'public_path' => 'blog',
+            'entry_provider' => \Bjuppa\LaravelBlog\Eloquent\BlogEntryProvider::class,
+        ]
+    ],
+    'eloquent_entries_table' => 'blog_entries',
 ];
