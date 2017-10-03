124265d791ea4d5ba0d04f864509e1dc1aba8e95
Björn Nilsved <nilsved@gmail.com>
2017-10-03 10:47:15 +0200

    Document table name config

--- config/blog.php
+++ config/blog.php
@@ -8,5 +8,18 @@ return [
             'entry_provider' => \Bjuppa\LaravelBlog\Eloquent\BlogEntryProvider::class,
         ]
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Table name for Eloquent blog entries
+    |--------------------------------------------------------------------------
+    |
+    | Blog entries are stored in a table called `blog_entries` by default. If
+    | you want another name for the table you can set it here. This table
+    | name will be used by the registered migrations and the BlogEntry
+    | model (and its descendants should you extend it).
+    |
+    */
+
     'eloquent_entries_table' => 'blog_entries',
 ];
