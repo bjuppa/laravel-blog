fcb8d54e53c0babb263589a7d06a4f571a48bc77
Björn Nilsved <nilsved@gmail.com>
2017-10-03 10:48:30 +0200

    Hardcode the default table name
    Don't use Eloquents model name as base for the table name

--- src/Eloquent/BlogEntry.php
+++ src/Eloquent/BlogEntry.php
@@ -16,9 +16,7 @@ class BlogEntry extends Eloquent implements BlogEntryContract
     {
         if (!isset($this->table)) {
             //TODO: Should the entry model's table name come from a config file specific to the eloquent blog entries?
-            return config('blog.eloquent_entries_table', function () {
-                return parent::getTable();
-            });
+            return config('blog.eloquent_entries_table', 'blog_entries');
         }
 
         return $this->table;
