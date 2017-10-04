34b0e62fa3c5aeb96a24c696ca9103cb367a0559
Björn Nilsved <nilsved@gmail.com>
2017-10-04 14:48:55 +0200

    Enable optional migrations in tests

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -45,6 +45,24 @@ abstract class IntegrationTest extends TestCase
      */
     protected function getEnvironmentSetUp($app)
     {
-        //
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+
+    /**
+     * Run the migrations registered in the app
+     */
+    protected function loadRegisteredMigrations()
+    {
+        $this->artisan('migrate');
+
+        $this->beforeApplicationDestroyed(function () {
+            $this->artisan('migrate:rollback');
+        });
     }
 }
