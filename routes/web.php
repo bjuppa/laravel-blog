18d0efb733ca5f201172a56b612d8e9db3724a4c
Björn Nilsved <nilsved@gmail.com>
2017-10-16 12:33:58 +0200

    Clean up using facades

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,16 @@
 <?php
 
 use Bjuppa\LaravelBlog\Contracts\Blog;
-use Bjuppa\LaravelBlog\Contracts\BlogRegistry;
+use Facades\Bjuppa\LaravelBlog\Contracts\BlogRegistry;
+use Illuminate\Support\Facades\Route;
 
-Route::middleware('web')->namespace('Bjuppa\LaravelBlog\Http\Controllers')->group(function () {
-    //TODO: pull the blog registry from a real-time facade instead of app()
-    $blog_registry = app()->make(BlogRegistry::class);
-    $blog_registry->all()->each(function (Blog $blog) {
-        $group = array_filter([
+Route::group(['middleware' => ['web'], 'namespace' => 'Bjuppa\LaravelBlog\Http\Controllers'], function () {
+    BlogRegistry::all()->each(function (Blog $blog) {
+        $group_attributes = array_filter([
             'domain' => $blog->getDomain(),
             'prefix' => $blog->getPublicPath()
         ]);
-        Route::group($group, function () use ($blog) {
+        Route::group($group_attributes, function () use ($blog) {
             Route::get('/', 'ListEntriesController@showIndex')->name($blog->prefixRouteName('index'));
             Route::get('{slug}', 'ShowEntryController')->name($blog->prefixRouteName('entry'));
         });
