1b9a0c455751a6c784a12fb5b328703a474add3f
Björn Nilsved <nilsved@gmail.com>
2017-10-26 17:06:07 +0200

    Remove title from blog feed meta link

--- resources/views/feed/meta_link.blade.php
+++ resources/views/feed/meta_link.blade.php
@@ -3,4 +3,4 @@
  * @var $blog \Bjuppa\LaravelBlog\Contracts\Blog
  */
 ?>
-<link href="{{ $blog->urlToFeed() }}" type="application/atom+xml" rel="alternate" title="Blog ATOM Feed"/>
+<link href="{{ $blog->urlToFeed() }}" type="application/atom+xml" rel="alternate" />
