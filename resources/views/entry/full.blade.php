31f08c335a6141d10d2843427253dc06a3c66d0e
Björn Nilsved <nilsved@gmail.com>
2017-10-27 14:33:21 +0200

    Add todos

--- resources/views/entry/full.blade.php
+++ resources/views/entry/full.blade.php
@@ -4,7 +4,10 @@
  */
 ?>
 <article>
+  {{-- TODO: use @includeFirst() here for special header display per blog and entry --}}
   @include('blog::entry.partials.header')
+  {{-- TODO: use @includeFirst() here for special footer display per blog and entry --}}
   @include('blog::entry.partials.footer')
+  {{-- TODO: use @includeFirst() here for special content display per blog and entry --}}
   @include('blog::entry.partials.content')
 </article>
--- resources/views/entry/partials/footer.blade.php
+++ resources/views/entry/partials/footer.blade.php
@@ -6,6 +6,7 @@
 <footer>
   <p>
     {{-- TODO: use the blog's default author if entry has no authors  --}}
+    {{-- TODO: use @includeFirst() here for special author display per blog and entry --}}
     @include('blog::entry.partials.authors', ['authors' => $entry->getAuthors()])
     <time datetime="{{ $entry->getPublished()->toAtomString() }}">{{ $entry->getPublished()->diffForHumans() }}</time>
   </p>
