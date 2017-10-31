bf8dca189a0343dd21fcf88467b26a3c3fb0557e
Björn Nilsved <nilsved@gmail.com>
2017-10-31 16:36:08 +0100

    Add todos

--- resources/views/entry/full.blade.php
+++ resources/views/entry/full.blade.php
@@ -7,4 +7,6 @@
   @includeFirst(['blog::entry.partials.header-'.$blog->getId().'-'.$entry->getId(), 'blog::entry.partials.header-'.$blog->getId(), 'blog::entry.partials.header'])
   @includeFirst(['blog::entry.partials.footer-'.$blog->getId().'-'.$entry->getId(), 'blog::entry.partials.footer-'.$blog->getId(), 'blog::entry.partials.footer'])
   @includeFirst(['blog::entry.partials.content-'.$blog->getId().'-'.$entry->getId(), 'blog::entry.partials.content-'.$blog->getId(), 'blog::entry.partials.content'])
+  {{-- TODO: add nav here with links to next and previous entries --}}
+  {{-- TODO: add aside here with links to latest entries & optional ads --}}
 </article>
