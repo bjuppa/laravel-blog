a6b0e0ace1f50907cada0b8bdd4342d065643145
Björn Nilsved <nilsved@gmail.com>
2017-11-01 16:00:51 +0100

    Add todo

--- resources/views/blog/header.blade.php
+++ resources/views/blog/header.blade.php
@@ -1,4 +1,10 @@
+<?php
+/**
+ * @var $blog \Bjuppa\LaravelBlog\Contracts\Blog
+ */
+?>
 <header>
-<h1>{{ $blog->getTitle() }}</h1>
-@includeFirst(['blog::blog.intro-'.$blog->getId(), 'blog::blog.intro'])
+  {{-- TODO: wrap this in a blade section blogHeader --}}
+  <h1>{{ $blog->getTitle() }}</h1>
+  @includeFirst(['blog::blog.intro-'.$blog->getId(), 'blog::blog.intro'])
 </header>
