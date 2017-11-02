770e30a20cff1cded0634dafa83f548371d40ec1
Björn Nilsved <nilsved@gmail.com>
2017-11-02 19:33:49 +0100

    Wrap the entry content in a <div> for styling/positioning
    It's not a <section> because it's not really a semantic section.
    It's actually just the bulk content of the containing <article>.

--- resources/views/entry/partials/content.blade.php
+++ resources/views/entry/partials/content.blade.php
@@ -3,7 +3,10 @@
  * @var $entry \Bjuppa\LaravelBlog\Contracts\BlogEntry
  */
 ?>
-{{-- TODO: consider adding a wrapping element around the entry content --}}
-<hr class="blog-content-start">
-{{ $entry->getContent() }}
-<hr class="blog-content-end">
+<div class="blog-entry-content">
+  <hr class="blog-content-start">
+
+  {{ $entry->getContent() }}
+
+  <hr class="blog-content-end">
+</div>
