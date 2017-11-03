cfeee96af06059bb91183976958568856f44b0de
Björn Nilsved <nilsved@gmail.com>
2017-11-03 17:09:01 +0100

    Make the publish data an english string

--- resources/views/entry/partials/footer.blade.php
+++ resources/views/entry/partials/footer.blade.php
@@ -6,9 +6,12 @@
 ?>
 <footer class="blog-entry-footer">
   @section('blogEntryFooter')
-    <p class="blog-entry-publication-info">
+    <p lang="en" class="blog-entry-publication-info">
+      Published
       <time datetime="{{ $entry->getPublished()->toAtomString() }}">{{ $entry->getPublished()->diffForHumans() }}</time>
+      in
       <a href="{{ $blog->urlToIndex() }}" rel="index">{{ $blog->getTitle() }}</a>
+      by
       @includeFirst(['blog::entry.partials.authors-'.$blog->getId().'-'.$entry->getId(), 'blog::entry.partials.authors-'.$blog->getId(), 'blog::entry.partials.authors'], ['authors' => $entry->getAuthors()->isEmpty() ? $blog->getAuthors() : $entry->getAuthors()])
     </p>
   @show
