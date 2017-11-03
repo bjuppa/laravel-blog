080a85a0118272d329de94c328eaf753d9328ff3
Björn Nilsved <nilsved@gmail.com>
2017-11-03 17:03:51 +0100

    Display published in short blog entry

--- resources/views/entry/short.blade.php
+++ resources/views/entry/short.blade.php
@@ -3,7 +3,15 @@
  * @var $entry \Bjuppa\LaravelBlog\Contracts\BlogEntry
  */
 ?>
-<article class="blog-entry">
-  <h2>{{ $entry->getTitle() }}</h2>
-  {{ $entry->getSummary() }}
+<article class="blog-entry-short">
+  <header class="blog-entry-header">
+    <h2 class="blog-entry-title">{{ $entry->getTitle() }}</h2>
+  </header>
+  <div class="blog-entry-summary">
+    {{ $entry->getSummary() }}
+  </div>
+  <footer class="blog-entry-footer">
+    <time datetime="{{ $entry->getPublished()->toAtomString() }}">{{ $entry->getPublished()->diffForHumans() }}</time>
+  </footer>
+  {{-- TODO: if image is to be added to short entries, it's probably here --}}
 </article>
