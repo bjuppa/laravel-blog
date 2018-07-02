648d4ff1ed5077a128b486c3f82bb7166ac2fa23
Björn Nilsved <nilsved@gmail.com>
2018-07-02 14:07:56 +0200

    Test fb sharing link

--- tests/Feature/SocialSharingTest.php
+++ tests/Feature/SocialSharingTest.php
@@ -19,5 +19,7 @@ class SocialSharingTest extends IntegrationTest
         $response = $this->get('blog/the-first-post');
 
         $response->assertSee('<li class="share-on-facebook">');
+        $response->assertSee('<a href="https://www.facebook.com/sharer.php?u=http%3A%2F%2Flocalhost%2Fblog%2Fthe-first-post" target="_blank" rel="noopener">');
+        $response->assertSee('><span>Share this page on </span><span>Facebook</span></a>');
     }
 }
