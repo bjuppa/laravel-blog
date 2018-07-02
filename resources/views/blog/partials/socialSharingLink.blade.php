67023fa3c8236a12dd7d6472b9836fe86894171b
Björn Nilsved <nilsved@gmail.com>
2018-07-02 14:36:02 +0200

    Move sharing link to own partial template

---
+++ resources/views/blog/partials/socialSharingLink.blade.php
@@ -0,0 +1,1 @@
+<a href="{{ $href }}" target="_blank" rel="noopener"><span class="share-link-prefix">{{ trans($blog->transKey('sharing.link_prefix')) }}</span><span class="share-link-service">{!! $service_name !!}</span></a>
--- resources/views/blog/partials/socialSharingListItem.blade.php
+++ resources/views/blog/partials/socialSharingListItem.blade.php
@@ -1,3 +1,3 @@
 <li class="share-on-{{ kebab_case(strip_tags($service_name)) }}">
-  <a href="{{ $href }}" target="_blank" rel="noopener"><span class="share-link-prefix">{{ trans($blog->transKey('sharing.link_prefix')) }}</span><span class="share-link-service">{!! $service_name !!}</span></a>
+  @includeFirst($blog->bladeViews('blog.partials.socialSharingLink'))
 </li>
