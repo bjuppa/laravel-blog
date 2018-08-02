484ddc17993310ddb4a9859670a6559d0e4ea1a2
Björn Nilsved <nilsved@gmail.com>
2018-08-02 11:13:41 +0200

    Make meta tags except charset overridable

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -1,10 +1,13 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-  @yield('meta', Bjuppa\MetaTagBag\MetaTagBag::make($metaTags ?? [])->merge(
-    ['charset' => 'utf-8'],
+  @yield('meta', Bjuppa\MetaTagBag\MetaTagBag::make(
     ['http-equiv' => 'X-UA-Compatible', 'content' => 'IE=edge'],
     ['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1']
+  )
+  ->merge($metaTags ?? [])
+  ->merge(
+    ['charset' => 'utf-8']
   ))
 
   <title>@yield('title', 'Blog')</title>
