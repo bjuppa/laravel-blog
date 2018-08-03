7078fdd8ad556cd36f5f734d353340f99a87e528
Björn Nilsved <nilsved@gmail.com>
2018-08-03 16:01:01 +0200

    Add ad for the blog package itself and it's author

--- resources/views/entry/partials/asideAds.blade.php
+++ resources/views/entry/partials/asideAds.blade.php
@@ -1,3 +1,10 @@
 @section('entryAsideAds')
-  {{-- TODO: place ads here --}}
+  <div>
+    <small>ADVERTISMENTS</small>
+    <div>
+      This blog is powered by
+      <a href="https://packagist.org/packages/bjuppa/laravel-blog" target="_blank" rel="noopener"><code>bjuppa/laravel-blog</code></a>
+      created by Björn Nilsved.
+    </div>
+  </div>
 @show
