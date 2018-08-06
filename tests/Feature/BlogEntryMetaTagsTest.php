d56063e530bc3a46fb353a1fee04147a60da0b22
Björn Nilsved <nilsved@gmail.com>
2018-08-06 12:21:31 +0200

    Add json type field to blog entries

---
+++ tests/Feature/BlogEntryMetaTagsTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Bjuppa\LaravelBlog\Tests\Feature;
+
+use Bjuppa\LaravelBlog\Eloquent\BlogEntry;
+use Bjuppa\LaravelBlog\Tests\IntegrationTest;
+use Bjuppa\MetaTagBag\MetaTagBag;
+
+class BlogEntryMetaTagsTest extends IntegrationTest
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->loadRegisteredMigrations();
+        $this->seedDefaultBlogEntry();
+    }
+
+    protected function setEntryMetaTags($meta_tags)
+    {
+        $entry = BlogEntry::first();
+        $entry->meta_tags = $meta_tags;
+        $entry->save();
+    }
+
+    public function test_null_meta_tags()
+    {
+        $entry = BlogEntry::first();
+        $originalTagBag = $entry->getMetaTagBag();
+
+        $this->setEntryMetaTags(null);
+
+        $this->assertEquals($originalTagBag, $entry->getMetaTagBag());
+    }
+
+    public function test_json_meta_tags()
+    {
+        $this->setEntryMetaTags('[{"a": "b"}]');
+
+        $response = $this->get('blog/the-first-post');
+
+        $response->assertSee('<meta a="b">');
+    }
+
+    public function test_invalidjson_meta_tags()
+    {
+        $this->setEntryMetaTags('[{"a": "b"},]');
+
+        $response = $this->get('blog/the-first-post');
+
+        $response->assertDontSee('<meta a="b">');
+    }
+
+    public function test_array_meta_tags()
+    {
+        $this->setEntryMetaTags(['a' => 'b']);
+
+        $response = $this->get('blog/the-first-post');
+
+        $response->assertSee('<meta a="b">');
+    }
+
+    public function test_bag_meta_tags()
+    {
+        $this->setEntryMetaTags(new MetaTagBag(['a' => 'b']));
+
+        $response = $this->get('blog/the-first-post');
+
+        $response->assertSee('<meta a="b">');
+    }
+}
