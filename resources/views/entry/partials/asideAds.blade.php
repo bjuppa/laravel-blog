19b38396a36c4dc7b7d312a9a724d3df7c170cf5
Björn Nilsved <nilsved@gmail.com>
2018-09-10 11:26:07 +0200

    Remove period from ad

--- resources/views/entry/partials/asideAds.blade.php
+++ resources/views/entry/partials/asideAds.blade.php
@@ -6,7 +6,7 @@
     <div>
       This blog is powered by
       <a href="https://packagist.org/packages/bjuppa/laravel-blog" target="_blank" rel="noopener"><code>bjuppa/laravel-blog</code></a>
-      created by Björn Nilsved.
+      created by Björn Nilsved
     </div>
   </section>
 @show
