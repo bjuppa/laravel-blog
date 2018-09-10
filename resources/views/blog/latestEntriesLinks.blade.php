51575a1b0406b254179efbc6223426615d82dd78
Björn Nilsved <nilsved@gmail.com>
2018-09-10 09:44:30 +0200

    Make latest entries an ordered list

--- resources/views/blog/latestEntriesLinks.blade.php
+++ resources/views/blog/latestEntriesLinks.blade.php
@@ -4,10 +4,10 @@
  * @var $entry \Bjuppa\LaravelBlog\Contracts\BlogEntry
  */
 ?>
-<ul class="blog-latest-entries-links" aria-label="{{ __($blog->transKey('titles.latest_entries')) }}">
+<ol class="blog-latest-entries-links" aria-label="{{ __($blog->transKey('titles.latest_entries')) }}">
   @foreach($blog->latestEntries() as $entry)
     <li>
       <a href="{{ $blog->urlToEntry($entry) }}" class="blog-entry-link">{{ $entry->getTitle() }}</a>
     </li>
   @endforeach
-</ul>
+</ol>
