1687717e1e348619275753af80f45a0c73500cfb
Björn Nilsved <nilsved@gmail.com>
2018-09-10 11:37:17 +0200

    Make ads narrower

--- resources/views/entry/partials/asideAds.blade.php
+++ resources/views/entry/partials/asideAds.blade.php
@@ -4,8 +4,8 @@
       <small>{{ __($blog->transKey('titles.ads')) }}</small>
     </header>
     <div>
-      This blog is powered by
-      <a href="https://packagist.org/packages/bjuppa/laravel-blog" target="_blank" rel="noopener"><code>bjuppa/laravel-blog</code></a>
+      This blog is powered by<br>
+      <a href="https://packagist.org/packages/bjuppa/laravel-blog" target="_blank" rel="noopener"><code>bjuppa/laravel-blog</code></a><br>
       created by Björn Nilsved
     </div>
   </section>
