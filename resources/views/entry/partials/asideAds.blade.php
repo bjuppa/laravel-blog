1018f861817ecfb7df1e1a498205d8c75f7df4ef
Björn Nilsved <nilsved@gmail.com>
2018-09-10 10:56:17 +0200

    Put ads in a <section>

--- resources/views/entry/partials/asideAds.blade.php
+++ resources/views/entry/partials/asideAds.blade.php
@@ -1,10 +1,12 @@
 @section('entryAsideAds')
-  <div>
-    <small>ADVERTISEMENTS</small>
+  <section>
+    <header>
+      <small>ADVERTISEMENTS</small>
+    </header>
     <div>
       This blog is powered by
       <a href="https://packagist.org/packages/bjuppa/laravel-blog" target="_blank" rel="noopener"><code>bjuppa/laravel-blog</code></a>
       created by Björn Nilsved.
     </div>
-  </div>
+  </section>
 @show
