ec9ea342915f8ff456a978b932f52c3a334d4761
Björn Nilsved <nilsved@gmail.com>
2018-09-12 08:19:51 +0200

    Use the Kingdom blog utils

--- resources/views/entry/partials/relatedContent.blade.php
+++ resources/views/entry/partials/relatedContent.blade.php
@@ -1,5 +1,5 @@
 @section('blogRelatedContent')
-  <aside class="blog-related-content">
+  <aside class="blog-related-content grid-row-span-6">
     @includeFirst($blog->bladeViews('blog.latestEntriesLinks'))
   </aside>
   @includeFirst($blog->bladeViews('entry.partials.asideAds'))
