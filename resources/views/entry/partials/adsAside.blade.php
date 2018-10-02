8302d8c5cbf2bfda9cffa1a8c016396c64afe309
Björn Nilsved <nilsved@gmail.com>
2018-10-02 15:35:41 +0200

    Set fonts on ads

--- resources/views/entry/partials/adsAside.blade.php
+++ resources/views/entry/partials/adsAside.blade.php
@@ -3,12 +3,12 @@
     <header>
       <small>{{ __($blog->transKey('titles.ads')) }}</small>
     </header>
-    <section>
+    <section style="font-family: Verdana,Geneva,sans-serif;">
       This blog is powered by<br>
       <a href="https://packagist.org/packages/bjuppa/laravel-blog" target="_blank" rel="noopener"><code>bjuppa/laravel-blog</code></a><br>
       created by Björn Nilsved
     </section>
-    <section>
+    <section style="font-family: Georgia,Times,Times New Roman,serif;">
       <p>
         Oppose fascism,<br>
         support democracy!
