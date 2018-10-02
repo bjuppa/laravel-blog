9220133efa9bb77e912f46ed0a9ced51a15fcaa2
Björn Nilsved <nilsved@gmail.com>
2018-10-02 10:50:30 +0200

    Add ad for democracy

--- resources/views/entry/partials/adsAside.blade.php
+++ resources/views/entry/partials/adsAside.blade.php
@@ -3,10 +3,14 @@
     <header>
       <small>{{ __($blog->transKey('titles.ads')) }}</small>
     </header>
-    <div>
+    <section>
       This blog is powered by<br>
       <a href="https://packagist.org/packages/bjuppa/laravel-blog" target="_blank" rel="noopener"><code>bjuppa/laravel-blog</code></a><br>
       created by Björn Nilsved
-    </div>
+    </section>
+    <section>
+      <p>Oppose fascism, support democracy!</p>
+      <p>Vote if you're allowed to where you live; and vote carefully!</p>
+    </section>
   </aside>
 @show
