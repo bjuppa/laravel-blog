e59077c4630543de0c0fe8bea1c4301a1f81fd5d
Björn Nilsved <nilsved@gmail.com>
2018-10-02 10:07:12 +0200

    Add header to latest entries

--- resources/views/entry/partials/relatedContent.blade.php
+++ resources/views/entry/partials/relatedContent.blade.php
@@ -1,5 +1,8 @@
 @section('blogRelatedContent')
   <aside class="blog-latest-entries">
+    <header>
+      <small>{{ __($blog->transKey('titles.latest_entries')) }}</small>
+    </header>
     @includeFirst($blog->bladeViews('blog.latestEntriesLinks'))
   </aside>
 @show
