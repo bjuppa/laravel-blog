44505929be4e05bd5508184e08526ef805bcedfd
Björn Nilsved <nilsved@gmail.com>
2018-10-02 15:50:35 +0200

    Edit democracy ad

--- resources/views/entry/partials/adsAside.blade.php
+++ resources/views/entry/partials/adsAside.blade.php
@@ -8,13 +8,13 @@
       <a href="https://packagist.org/packages/bjuppa/laravel-blog" target="_blank" rel="noopener"><code>bjuppa/laravel-blog</code></a><br>
       created by Björn Nilsved
     </section>
-    <section style="font-family: Georgia,Times,Times New Roman,serif;">
+    <section style="font-family: Georgia,Times,Times New Roman,serif;" class="p-1">
       <p>
         Oppose fascism,<br>
         support democracy!
       </p>
       <p>
-        Vote if you're allowed to where you live;<br>
+        Vote if you're allowed to;<br>
         and vote carefully!
       </p>
     </section>
