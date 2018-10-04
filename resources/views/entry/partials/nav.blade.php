aba09a0bb59c517cb886e5f94bb0891cc2b1b7f4
Björn Nilsved <nilsved@gmail.com>
2018-10-04 13:35:44 +0200

    Remove microdata from plain links to other entries

--- resources/views/entry/partials/nav.blade.php
+++ resources/views/entry/partials/nav.blade.php
@@ -10,15 +10,15 @@ $previousEntry = $blog->previousEntry($entry);
 <nav class="blog-entry-nav">
   @section('blogEntryNav')
     @if($nextEntry)
-      <a href="{{ $blog->urlToEntry($nextEntry) }}" rel="next" class="blog-entry-link" itemscope itemtype="http://schema.org/BlogPosting">
+      <a href="{{ $blog->urlToEntry($nextEntry) }}" rel="next" class="blog-entry-link">
         <small>{{ __($blog->transKey('titles.next')) }}<span>:</span></small>
-        <span itemprop="headline">{{ $nextEntry->getTitle() }}</span>
+        <span>{{ $nextEntry->getTitle() }}</span>
       </a>
     @endif
     @if($previousEntry)
-      <a href="{{ $blog->urlToEntry($previousEntry) }}" rel="prev" class="blog-entry-link" itemscope itemtype="http://schema.org/BlogPosting">
+      <a href="{{ $blog->urlToEntry($previousEntry) }}" rel="prev" class="blog-entry-link">
         <small>{{ __($blog->transKey('titles.previous')) }}<span>:</span></small>
-        <span itemprop="headline">{{ $previousEntry->getTitle() }}</span>
+        <span>{{ $previousEntry->getTitle() }}</span>
       </a>
     @endif
   @show
