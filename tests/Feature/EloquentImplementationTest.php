42897adb6408942e32702b7810b7b2eb619dfdd9
Björn Nilsved <nilsved@gmail.com>
2018-10-23 13:50:05 +0200

    Test that global scope excludes unpublished entries

---
+++ tests/Feature/EloquentImplementationTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Bjuppa\LaravelBlog\Tests\Feature;
+
+use Bjuppa\LaravelBlog\Eloquent\BlogEntry;
+use Bjuppa\LaravelBlog\Tests\IntegrationTest;
+
+class EloquentImplementationTest extends IntegrationTest
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->prepareDatabase();
+        $this->seedDefaultBlogEntry();
+    }
+
+    public function test_global_scope_excludes_unpublished_entries()
+    {
+        $entry = factory(BlogEntry::class)->create();
+
+        $this->assertFalse($entry->isPublic());
+        $this->assertCount(1, BlogEntry::all());
+        $this->assertCount(0, BlogEntry::all()->reject->isPublic());
+    }
+}
