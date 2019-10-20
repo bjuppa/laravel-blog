b20b428ad7817daecece22aefd49ce20ee61d474
Björn Nilsved <nilsved@gmail.com>
2019-10-20 15:43:14 +0200

    Style subscribe links

--- resources/views/feed/subscriptionLink.blade.php
+++ resources/views/feed/subscriptionLink.blade.php
@@ -3,6 +3,6 @@
  * @var $blog \Bjuppa\LaravelBlog\Contracts\Blog
  */
 ?>
-<a aria-label="{{ __($blog->transKey('feed.atom_subscribe'), ['blog' => $blog->getTitle()]) }}"
-  href="{{ $blog->urlToFeed() }}" type="application/atom+xml"
-  rel="alternate">{{__($blog->transKey('feed.subscribe'))}}</a>
+<small><a aria-label="{{ __($blog->transKey('feed.atom_subscribe'), ['blog' => $blog->getTitle()]) }}"
+    href="{{ $blog->urlToFeed() }}" type="application/atom+xml"
+    rel="alternate">{{__($blog->transKey('feed.subscribe'))}}</a></small>
