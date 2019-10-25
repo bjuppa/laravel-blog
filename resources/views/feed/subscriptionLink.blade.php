660afdd2c32668145d01ef3fcc0ec72d0260ae00
Björn Nilsved <nilsved@gmail.com>
2019-10-25 15:25:13 +0200

    Add word break opportunities

--- resources/views/feed/subscriptionLink.blade.php
+++ resources/views/feed/subscriptionLink.blade.php
@@ -5,4 +5,4 @@
 ?>
 <small><a aria-label="{{ __($blog->transKey('feed.atom_subscribe'), ['blog' => $blog->getTitle()]) }}"
     href="{{ $blog->urlToFeed() }}" type="application/atom+xml"
-    rel="alternate">{{__($blog->transKey('feed.subscribe'))}}</a></small>
+    rel="alternate"><wbr>{{__($blog->transKey('feed.subscribe'))}}<wbr></a></small>
