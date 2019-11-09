4f78e6c14af5f7439119d3769331b711f0826fee
Björn Nilsved <nilsved@gmail.com>
2019-11-09 16:56:59 +0100

    Remove instruction to run mix() in config files
    ...it doesn't work with config caching :(
    Modify the config within a service provider instead.

--- config/blog.php
+++ config/blog.php
@@ -54,9 +54,6 @@ return [
      */
 
     'blog_defaults' => [
-        /*
-         * Stylesheets may be run through mix() here in the config
-         */
         'stylesheets' => ['css/app.css'],
         //'timezone' => 'UTC', // For best results, use a named timezone like 'Europe/Stockholm'
         //'full_entries_in_feed' => false,
