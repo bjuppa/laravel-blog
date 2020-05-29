710b12a83f0313cba3ae965a29936d6132311994
Björn Nilsved <nilsved@gmail.com>
2020-05-29 14:12:29 +0200

    Add failing test for #64

--- tests/Feature/BlogEntryImageTest.php
+++ tests/Feature/BlogEntryImageTest.php
@@ -21,6 +21,29 @@ class BlogEntryImageTest extends IntegrationTest
         $entry = BlogEntry::first();
         $entry->image = $content;
         $entry->save();
+
+        return $entry;
+    }
+
+    public function test_image_url_can_be_extracted_from_string()
+    {
+        $entry = $this->setEntryImage($this->example_image_url);
+
+        $this->assertEquals($this->example_image_url, $entry->getImageUrl());
+    }
+
+    public function test_image_url_can_be_extracted_from_markdown()
+    {
+        $entry = $this->setEntryImage('![Alt text](' . $this->example_image_url . ')');
+
+        $this->assertEquals($this->example_image_url, $entry->getImageUrl());
+    }
+
+    public function test_image_url_can_be_extracted_from_img_tag()
+    {
+        $entry = $this->setEntryImage('<img src="' . $this->example_image_url . '" alt="Example image">');
+
+        $this->assertEquals($this->example_image_url, $entry->getImageUrl());
     }
 
     public function test_image_url()
