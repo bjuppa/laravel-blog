10e7d412270ba4e04c4cf33d00d63ce42addc443
Björn Nilsved <nilsved@gmail.com>
2020-09-18 10:25:11 +0200

    Replace underscores in html lang attribute

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   @yield('meta', Bjuppa\MetaTagBag\MetaTagBag::make(config('blog.default_meta_tags'))->merge($metaTags ?? [])->merge(['charset' => 'utf-8']))
 
